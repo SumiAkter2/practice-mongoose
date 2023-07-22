7cfd8d7fc60ee617200758ad877d56b2ed0c3bde
SumiAkter2 <akteralisha36@gmail.com>
2023-07-22 10:29:16 +0600

    create schema

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, NextFunction, Request,Response } from 'express';
 import cors from 'cors';
+import { Schema } from 'mongoose';
 
 const app:Application = express();
 app.use(cors());
@@ -18,7 +19,7 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     
     interface IUser{
         id: string;
-        roll: 'student';
+        role: 'student';
         password: string;
         name: {
             firstName: string;
@@ -34,7 +35,19 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
         permanentAddress: string;
         
   }
-
+    const userSchema = new Schema<IUser>({
+    id: { type: String, required: true, unique: true },  
+        role: { type: String, required: true },   
+     password: { type: String, required: true },
+        name: { firstName: { type: String, required: true },middleName:{ type: String },lastName:{type: String, required: true} },
+ dateOfBirth: { type: String,  },
+        gender: { type: String, enum:['male','female'] },
+        email: { type: String,  },
+        contactNo:{ type: String, required:true },
+        emergencyContactNo: { type: String, required:true },
+        presentAddress: { type: String, required:true },
+        permanentAddress: { type: String, required:true },
+});
 
     // res.send('Hello World!')
     // next();
