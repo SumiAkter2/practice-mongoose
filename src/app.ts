340dacd62c750b5b4e969bed0254ee31dd462ed7
SumiAkter2 <akteralisha36@gmail.com>
2023-07-22 10:02:21 +0600

    create interface

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,41 @@ import express, { Application, NextFunction, Request,Response } from 'express';
 import cors from 'cors';
 
 const app:Application = express();
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-app.get('/', (req:Request, res:Response,next:NextFunction) => {
-    res.send('Hello World!')
-    next();
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    // inserting a test data into mongodb
+
+    /*
+    1.interface
+    2.schema
+    3.model
+    4.database query
+    */
+    
+    interface IUser{
+        id: string;
+        roll: 'student';
+        password: string;
+        name: {
+            firstName: string;
+            middleName: string;
+            lastName: string;
+        }
+        dateOfBirth?: string;
+        gender: 'male' | 'female';
+        email?: string;
+        contactNo: string;
+        emergencyContactNo: string;
+        presentAddress: string,
+        permanentAddress: string;
+        
+  }
+
+
+    // res.send('Hello World!')
+    // next();
 })
 export default app;
